4da5341610e20f788169eccc865ad2460012a819
Antonia Majdandzic <tonka3aml@gmail.com>
2026-01-21 12:24:16 +0100

    stranice

---
+++ src/components/Test.js
@@ -0,0 +1,12 @@
+import "./Test.css";
+
+const Test = () => {
+  return (
+    <>
+      <p className="danger">Hello World-2</p>
+      <>Nesto novo</>
+    </>
+  );
+};
+
+export default Test;
---
+++ src/pages/Blog.js
@@ -0,0 +1,14 @@
+
+const Blog = () => {
+  return (
+    
+    <div className="blog-page">
+         <div className="container">
+        <h1>Blog</h1>
+      </div>
+   </div>
+    
+  );
+};
+
+export default Blog;
